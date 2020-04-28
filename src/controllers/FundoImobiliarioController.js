06643d2a06e9c23b8a050da665ec2de0bbcde0b3
= <roliveirasv@gmail.com>
2020-04-28 19:37:26 -0300

    construção dos métodos da controller e rotas

---
+++ src/controllers/FundoImobiliarioController.js
@@ -0,0 +1,76 @@
+const FundoImobiliario = require('../models/FundoImobiliario');
+const TOKEN='123456'
+
+const FundoImobiliarioController  = {
+
+home: async (req, res, next) => {
+    return res.status(200).json({info: 'API Fundo Imobiliario - Million - protegido com token'
+        })
+    },
+index: async(req,res,next)=>{
+    if(req.headers.token == TOKEN){
+        try{
+            const fundoImob = await FundoImobiliario.find({});
+            return res.status(200).send(fundoImob);
+        }catch(err){
+            return res.status(401).send(err);
+
+        }
+    }
+    return res.status(401).json({error: 'Acesso negado'});
+},
+
+
+create: async (req, res, next) => {
+    if (req.headers.token === TOKEN) {
+        const { nome, taxaRetMensal, indiceIfix } = req.body
+        try{
+
+            const fundoImob = await FundoImobiliario.create({ 
+            nome,
+            taxaRetMensal,
+            indiceIfix 
+        })
+        return res.status(201).send(fundoImob);
+
+        }catch(err){
+            return res.status(401).send(err);
+        }
+    }
+    return res.status(401).json({error: 'Acesso negado'});
+},
+
+
+change: async (req, res, next) => {
+    if (req.headers.token === TOKEN) {
+        const{nome, taxaRetMensal, indexIfix} = req.body
+        const{_id} = req.params
+        try{
+            await FundoImobiliario.findOneAndUpdate(
+                {_id:_id},{nome:nome,taxaRetMensal:taxaRetMensal,indiceIfix:indiceIfix})
+            return res.status(201).send('Alterado com sucesso');
+            
+        }catch(err){
+            return res.status(401).send(err);
+        }
+    }
+    return res.status(401).json({error: 'Acesso negado'});
+},
+
+delete: async (req, res, next) => {
+    if (req.headers.token === TOKEN) {
+     try{
+        await FundoImobiliario.findByIdAndDelete(req.params._id)
+        return res.status(201).send({});
+        
+        }catch(err){
+            return res.status(401).send(err);
+        }
+
+    }
+    return res.status(401).json({error: 'Acesso negado'});
+}
+
+}
+
+module.exports = FundoImobiliarioController
