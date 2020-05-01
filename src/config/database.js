5c0d0ac2931d26fa4b1190aadc1e234db11e6a15
Danilo <didox_59@yahoo.com.br>
2020-04-30 23:32:50 -0300

    incluindo database por ambiente

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const uri = 'mongodb://localhost:27017/FI-Million';
+const uri = process.env.DATABASE_URL ? process.env.DATABASE_URL : 'mongodb://localhost:27017/FI-Million';
 
 mongoose.connect(uri,  { 
     useNewUrlParser: true, 
